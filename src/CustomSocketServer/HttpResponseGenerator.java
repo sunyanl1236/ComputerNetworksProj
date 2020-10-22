fb684e7859a1c36ad47fcd773091b8816fbef3ae
yilu.liang <yiluliang1236@gmail.com>
2020-10-22 00:11:55 -0400

    modified response generator file

--- src/CustomSocketServer/HttpResponseGenerator.java
+++ src/CustomSocketServer/HttpResponseGenerator.java
@@ -1,6 +1,18 @@
 package CustomSocketServer;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.NotDirectoryException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.nio.file.attribute.FileTime;
 import java.time.LocalDate;
+import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 import java.util.Date;
 import java.util.HashMap;
@@ -15,7 +27,7 @@ public class HttpResponseGenerator {
 		private int statusCode;
 		private HashMap<String, String> resHeader = new HashMap<>();
 		private HashMap<Integer, String> statusCodePhrase = new HashMap<>();
-		private String resBody;
+		private String resBody="";
 		
 		//constructor
 		private HttpResponseGenerator() {
@@ -51,7 +63,101 @@ public class HttpResponseGenerator {
 			return this.statusCode;
 		}
 		
-		public void processRequest(String queryDir) {
+		//return the status code
+		public void processRequest(String requestMethod,String queryDir, String rootDir, boolean hasOverwrite) {
+			if(requestMethod.toUpperCase().equals("GET")) {
+				processGetReq(queryDir, rootDir);
+			}
+			else if(requestMethod.toUpperCase().equals("POST")) {
+				
+			}
+			else {
+				this.statusCode = 400;
+			}
+		}
+		
+		//return the status code
+		public void processGetReq(String queryDir, String rootDir) {
+			Path path = Paths.get(rootDir+queryDir);
+			System.out.println("Full path is "+rootDir+queryDir);
+			
+			try {
+				if(Files.isDirectory(path)) { //check if the path is a dir
+//				System.out.println("It's a directory.");
+					if(Files.isReadable(path)) { //if readable
+						//print all the dir and files in this path
+						DirectoryStream<Path> dirStream = Files.newDirectoryStream(path);
+						for (Path entry : dirStream) {
+							this.statusCode = 200;
+							resBody += "The request path is a directory, print all files and directories in the path: ";
+							resBody += (entry.getFileName().toString()+"/n");  
+						}
+						
+						//add Content-Length header
+						resHeader.put("Content-Length", Integer.toString(this.resBody.length()));
+						
+						//add Content-Type header
+						resHeader.put("Content-Type", "text/plain");
+						
+						System.out.println(resBody);
+					}
+					else { //if not readable
+						this.statusCode = 403;
+						System.out.println("Dir not readable.");
+					}
+				}// end check dir
+				else if(Files.isRegularFile(path)) { //check if the path is a file
+					if(Files.isReadable(path)) { //check permission of a file
+						System.out.println("Is a file.");
+						String mimeType = Files.probeContentType(path);
+						
+//						BasicFileAttributes attr = Files.readAttributes(path, BasicFileAttributes.class);
+//						FileTime lastModified = attr.lastModifiedTime();
+//						DateTimeFormatter formatter = DateTimeFormatter.RFC_1123_DATE_TIME;
+//						String dataString = formatter.withZone(ZoneId.systemDefault()).format(FileTime.from(0, TimeUnit.MILLISECONDS).toInstant());
+						
+						//read the file content
+						File f = path.toFile();
+						BufferedReader br = new BufferedReader(new FileReader(f));
+						StringBuilder sb = new StringBuilder();
+						String line;
+						
+						while((line = br.readLine()) != null) {
+							sb.append(line);
+							sb.append(System.lineSeparator());
+				        }
+						
+						this.resBody = sb.toString();
+						
+						//add Content-Length header
+						resHeader.put("Content-Length", Integer.toString(this.resBody.length()));
+						//add Content-Type header
+						resHeader.put("Content-Type", mimeType);
+						
+						System.out.println(resBody);
+					}
+					else {
+						this.statusCode = 403;
+						System.out.println("File not readable.");
+					}
+				} //end check file
+				else { //cannot open the path
+					this.statusCode = 404;
+					System.out.println("Cannot open the path");
+				}
+			}
+			catch(NotDirectoryException e) {
+				System.out.println("Dir E in processGetReq");
+				e.printStackTrace();
+			}
+			catch(IOException e) {
+				System.out.println("IO E in processGetReq");
+				e.printStackTrace();
+			}
+		}
+		
+		//*** implement overwrite options in Client
+		public void processPostReq(String queryDir, String rootDir, boolean hasOverwrite) {
 			
 		}
 		
@@ -71,28 +177,14 @@ public class HttpResponseGenerator {
 			DateTimeFormatter formatter = DateTimeFormatter.RFC_1123_DATE_TIME;
 			String dataString = date.format(formatter);
 			sb.append("Date: ").append(dataString).append("\r\n");
-			
-/* ****Last-Modified: 
- * Path file = Paths.get(fileName);
-            BasicFileAttributes attr =
-                Files.readAttributes(file, BasicFileAttributes.class);
-
-            System.out.println("creationTime: " + attr.creationTime());
-            System.out.println("lastAccessTime: " + attr.lastAccessTime());
-            System.out.println("lastModifiedTime: " + attr.lastModifiedTime());
- * */
-           
+     
 			sb.append("Server: Concordia Server-HTTP/1.0\r\n");
 			sb.append("MIME-version: 1.0\r\n");
-			//****Content-Length for response entity length
-			//****Content-Type get from request client
 			
 			//blank line
 			sb.append("\r\n");
 			
-			/* check GET and POST request body in main
-			 * the GET method should not have entity body
-			 * */
+			//entity
 			if(!this.resBody.isEmpty()) {
 				sb.append(this.resBody);
 			}
